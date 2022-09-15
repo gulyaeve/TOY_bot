c24b162932f48b5e0eb77b1764ebac481e70fe3d
gulyaeve <gulyaev.e@icloud.com>
2022-09-15 16:17:52 +0300

    Added check for finalists before start web

--- handlers/managers/start_web.py
+++ handlers/managers/start_web.py
@@ -3,15 +3,21 @@ from logging import log, INFO
 from aiogram import types
 
 from filters import ManagerCheck
-from loader import dp, teachers
+from loader import dp, teachers, messages
 
 
 @dp.message_handler(ManagerCheck(), commands=['start_web'])
 async def start_web(message: types.Message):
-    json = await teachers.get_json_with_finalists()
-    log(INFO, json)
-    await teachers.update_ug2022(json)
-    await message.answer(f"Смотри анимацию")
+    all_finalists = await teachers.select_all_finalists()
+    max_finalists = await teachers.select_parameter('max_finalists')
+    if all_finalists.__len__() == int(max_finalists):
+        json = await teachers.get_json_with_finalists()
+        log(INFO, json)
+        await teachers.update_ug2022(json)
+        await message.answer(f"Смотри анимацию")
+    else:
+        answer = await messages.get_message("not_finalists")
+        await message.answer(answer.format(all_finalists.__len__(), max_finalists))
 
 
 @dp.message_handler(ManagerCheck(), commands=['stop_web'])
