5904943a659fd3e3119fe94db7c541ba533ce41c
gulyaeve <gulyaev.e@icloud.com>
2022-09-21 12:19:58 +0300

    Added third status for poll

--- handlers/admins/clear_vote.py
+++ handlers/admins/clear_vote.py
@@ -7,4 +7,5 @@ from loader import dp, teachers
 @dp.message_handler(AdminCheck(), commands=['clear_votes'])
 async def clear_votes(message: types.Message):
     await teachers.clear_votes()
+    await teachers.update_ug2022_3_on()
     await message.answer('Голоса очищены')
